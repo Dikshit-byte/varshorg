66a4d67ec5620b002b2d7cbbadff27c0a958706f
Dikshit <singhdikshit69@gmail.com>
2022-04-20 16:33:13 +0530

    change the card section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Navbar1 from './components/TopNavbar/Navbar'
 import Hero from './components/Hero/Hero'
+import Temp from './components/TemplateShowCase/Temp'
+import Carousal from './components/Carousal/Carousal'
+import Contact from './components/ContactUsPage/Contact'
 const App = () => {
   return (
     <>
@@ -9,6 +12,11 @@ const App = () => {
     {/* <ContactUs/> */}
     <Navbar1/>
     <Hero/>
+    <Temp/>
+    <div className='carousalDiv ' >
+    <Carousal/>
+    </div>
+    <Contact/>
     </>
   )
 }
